8beed5e18372fe26290a47e1efd319ee61c62504
ouyp <48140675+vron8632@users.noreply.github.com>
2023-12-25 09:57:40 +0800

    Delete _base_ directory

--- _base_/models/upernet_ch256_mit.py
+++
@@ -1,8 +0,0 @@
-# ---------------------------------------------------------------
-# Copyright (c) 2021-2022 ETH Zurich, Lukas Hoyer. All rights reserved.
-# Licensed under the Apache License, Version 2.0
-# ---------------------------------------------------------------
-
-_base_ = ['upernet_mit.py']
-
-model = dict(decode_head=dict(channels=256, ))
